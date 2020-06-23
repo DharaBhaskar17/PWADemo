6668cedf2b9db77966c5f75d655bd24b2f14cc1f
DharaBhaskar17 <dhara13bhaskar@gmail.com>
2020-06-23 19:44:29 +0530

    PWADemo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jokes';
+
+  update: boolean = false;
+  joke: any;
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+      console.log(event);
+      updates.activateUpdate().then(() => document.location.reload());
+    })
+  }
+
+  ngOnInit(): void {
+    this.data.jokes().subscribe(res => {
+      this.joke = res['value'];
+      console.log(this.joke);
+    })
+  }
+
+
 }
